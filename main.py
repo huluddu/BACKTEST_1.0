767da5e7d57ee96b0f7e43fe2257320ac77010b9
huluddu <73523299+huluddu@users.noreply.github.com>
2026-04-13 23:48:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import random
 import google.generativeai as genai
+import optuna
 
 # 모듈 불러오기
 from modules.utils import load_saved_strategies, save_strategy_to_file, delete_strategy_from_file, parse_choices
