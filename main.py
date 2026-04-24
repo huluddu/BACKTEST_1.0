a1cd6bf11fb4b06dc94bf73470d18a0583abe458
huluddu <73523299+huluddu@users.noreply.github.com>
2026-04-24 22:41:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1221,6 +1221,7 @@ with tab7:
                 study.optimize(lambda trial: optuna_objective(
                     trial, base_full, x_sig_full, x_trd_full, ma_dict, 
                     safe_cash, safe_fee, safe_slip, safe_behavior, safe_hold, target_score
+                    disable_tp=disable_tp_checkbox
                 ), n_trials=n_trials)
                 
                 # 💡 [핵심 해결책] 찾아낸 결과를 st.session_state에 저장!
